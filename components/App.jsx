e450af9a9da660a9403ce2b904e1877398865ce7
michaelle7 <michael.le01@edu.sait.ca>
2024-10-11 20:06:38 -0600

    did lab2

---
+++ components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import ToDoList from './components/ToDoList';
+import ToDoForm from './components/ToDoForm';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ToDoList />
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+
+export default App;
