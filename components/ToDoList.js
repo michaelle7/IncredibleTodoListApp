4d3691f81d1e57dda653939bf0658ababd9345ad
michaelle7 <michael.le01@edu.sait.ca>
2024-11-01 20:57:39 -0600

    added task to todolist

--- components/ToDoList.js
+++ components/ToDoList.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import { ScrollView, Pressable, View, Text, StyleSheet } from 'react-native';
 
-export default function ToDoList() {
+export default function ToDoList({ tasks }) {
   return (
     <ScrollView>
-      <Pressable>
-        <View style={[styles.task, styles.completed]}>
-          <Text style={styles.taskText}>Do laundry</Text>
-        </View>
-      </Pressable>
-      <Pressable>
-        <View style={[styles.task]}>
-          <Text style={styles.taskText}>Go to gym</Text>
-        </View>
-      </Pressable>
-      <Pressable>
-        <View style={[styles.task, styles.completed]}>
-          <Text style={styles.taskText}>Walk dog</Text>
-        </View>
-      </Pressable>
+      {tasks.map((task, index) => (
+        <Pressable key={index}>
+          {/* Display each task without the completed style */}
+          <View style={styles.task}>
+            <Text style={styles.taskText}>{task}</Text>
+          </View>
+        </Pressable>
+      ))}
     </ScrollView>
   );
 }
